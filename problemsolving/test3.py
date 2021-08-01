691e635ddfa0269ee1831c94f0756a6b37513250
Swan31 <36279280+Swan31@users.noreply.github.com>
2021-08-01 18:57:05 +0200

    Dokončení test3.py

--- problemsolving/test3.py
+++ problemsolving/test3.py
@@ -1,48 +1,42 @@
+import hashlib
+
 # Zjistit zda dva string obsajují stejná písmena - ve stejném počtu, nikoli v pořadí.
 # Porovnání stringů -
-# 1. TODO: Hash - https://www.pythoncentral.io/hashing-strings-with-python/
-# 2. TODO: Délka
+# 1. Hash - https://www.pythoncentral.io/hashing-strings-with-python/
+# 2. Délka
+# 3. Typ a počet písmen ve slovech
 
 word1 = "MOR"
 word2 = "ROM"
 
 
-# fjk
-# fld
-#
-# ffk
-# ffd
-#
-# fgfgf
-# fdgf
-
-def zip_it(my_word1, my_word2):
-    result = zip(my_word1, my_word2)
-    result_set = list(result)
-    unzip_result = zip(*result_set)
-    print(result_set)
-    print(set(unzip_result))
-
-
-def get_letters(word):
-    letters = {}
-    for letter in word:
-        if letter in letters:
-            letters.update({letter: letters["letter"] + 1})
+def compare_words(my_word1, my_word2):
+    if compare_words_hash(my_word1, my_word2):
+        return True
+    if len(my_word1) != len(my_word2):
+        print("Len")
+        return False
+    letters1, letters2 = {}, {}
+    for letter1, letter2 in zip(my_word1, my_word2):
+        if letter1 in letters1:
+            letters1.update({letter1: letters1["letter1"] + 1})
         else:
-            letters.update({letter: 1})
-            # letters[letter] = 1
-    return letters
-
-
-def compare(my_word1, my_word2):
-    letters1 = get_letters(my_word1)
-    letters2 = get_letters(my_word2)
-    print(letters1)
-    print(letters2)
+            letters1.update({letter1: 1})
+        if letter2 in letters2:
+            letters2.update({letter2: letters2["letter2"] + 1})
+        else:
+            letters2.update({letter2: 1})
     match = letters1 == letters2
     print(match)
 
 
-# compare(word1, word2)
-zip_it(word1, word2)
+def compare_words_hash(my_word1, my_word2):
+    hash1 = hashlib.sha512(my_word1.encode())
+    hash2 = hashlib.sha512(my_word2.encode())
+    hex_dig1 = hash1.hexdigest()
+    hex_dig2 = hash2.hexdigest()
+    match = hex_dig1 == hex_dig2
+    return match
+
+
+compare_words(word1, word2)
